d6d798c079e6fad4c57ccc43a02a28067614e911
hbach24 <57569284+hbach24@users.noreply.github.com>
2022-12-07 03:10:47 -0800

    Implemented Forward Selection Search

--- featureSelection.py
+++ featureSelection.py
@@ -1,23 +1,24 @@
 #Summary of the project: search through feature tree and find the set of feature(s) that provide the best accuracy
 import csv 
+import random
 
 def main():
     print("Welcome to Hannah Bach's Feature Selection Algorithm.")
-    # search = input("Type the number of the algorithm you want to run: \
-    # \nEnter '1' to use Forward Selection. \nEnter '2' to use Backward Elimination.\n")
-    search  = 0 #delete this later and uncomment lines 6&7
+    search = input("Type the number of the algorithm you want to run: \
+    \nEnter '1' to use Forward Selection. \nEnter '2' to use Backward Elimination.\n")
+    # search  = 0 #delete this later and uncomment lines 6&7
 
-    # file = "Small_Data_6.txt"
+    file = "Small_Data_6.txt"
     # file = "Small_Data_88.txt"
     # file = "Small_Data_96.txt"
     # file = "Large_Data_6.txt" #41 columns, 40 feature columns
-    file = "Large_Data_21.txt"
+    # file = "Large_Data_21.txt"
     # file = "Large_Data_96.txt"
     data = getData(file)
 
     if(search == '1'):
         searchName = "Forward Selection"
-        forwardSelectionSearch(1)
+        forwardSelectionSearch(data)
 
     if(search == '2'):
         searchName = "Backward Elimination"
@@ -46,13 +47,41 @@ def getData(file):
                 dataRow.append(float(rawRow[j])) #convert scientific notation to float; REFERENCE: https://stackoverflow.com/questions/23636509/convert-string-in-scientific-notation-to-float
             # print(dataRow)
             data.append(dataRow)
-        print(data)
+        # print(data)
     return data
 
-
+def leaveOneOutCrossValidation(data, currentFeatureSet, featureToAdd):
+    acc = random.random()
+    return acc
 
 def forwardSelectionSearch(data):
-    print("forward")
+    numFeatures = len(data[0])
+    # print(numFeatures-1)
+
+    currentFeatureSet = []
+   
+    for i in range(1,numFeatures): #disregard the first column since it's not a feature but a class
+        print(f"On the {i}th level of the search tree.")
+        featureToAdd = 0
+        bestAccuracy = 0
+        
+        for k in range(1,numFeatures):
+
+            if(k not in currentFeatureSet): #don't add a feature that's already in the current feature set
+                print(f"--Considering adding the {k} feature.")
+                accuracy = leaveOneOutCrossValidation(data, currentFeatureSet, k)
+
+                if(accuracy > bestAccuracy):
+                    bestAccuracy = accuracy
+                    featureToAdd = k
+        
+#level 1: currentFeatureSet = [4] ; level 2: currentFeatureSet = [4,5] 
+            # feature = data[i][j]
+        currentFeatureSet.append(featureToAdd)
+        print(f"On level {i}, I added feature {featureToAdd} to the current feature set.")
+        print(f"Current Feature Set at level {i}:", currentFeatureSet, "\n")
+
+
 
 def backwardEliminationSearch(data):
     print("backward")
